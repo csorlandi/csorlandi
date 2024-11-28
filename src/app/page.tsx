964b02290457f42f5583db463b96039a51453baf
Claudio Orlandi <claudiosorlandi@gmail.com>
2024-11-28 16:28:43 +0100

    feat: implement hero section content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "@/sections/Header";
+import { HeroSection } from "@/sections/Hero";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <HeroSection />
     </main>
   );
 }
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,27 @@
+import memojiImage from '@/assets/images/memoji-computer.png';
+import ArrowDown from '@/assets/icons/arrow-down.svg';
+import Image from 'next/image';
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className="container py-32 md:py-48 lg:py-60 flex flex-col items-center md:max-w-xl">
+      <Image src={memojiImage} className="size-24" alt="Person peeking from behind laptop" />
+      <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+        <span className="bg-green-500 size-2.5 rounded-full" />
+        <p className="text-sm font-medium">Available for new projects</p>
+      </div>
+      <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">Building Exceptional User Experiences</h1>
+      <p className="mt-4 text-center text-white/60 md:text-lg">I specialize in transforming designs into functional, high-performing web applications. Let&apos;s discuss your next project.</p>
+      <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+        <button type="button" className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+          <span className="font-semibold">Explore My Work</span>
+          <ArrowDown className="size-4" />
+        </button>
+        <button type="button" className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+          <span>👋</span>
+          <span className="font-semibold">Let&apos;s connect</span>
+        </button>
+      </div>
+    </div>
+  );
 };
