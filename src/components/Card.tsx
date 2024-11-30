cd0de60884ca4b092beddf79657e2c4ad332ea7b
Claudio Orlandi <claudiosorlandi@gmail.com>
2024-12-01 00:54:59 +0100

    feat: create reusable card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,18 @@
+import grainImage from '@/assets/images/grain.jpg';
+import { PropsWithChildren } from 'react';
+import { ClassNameValue, twMerge } from 'tailwind-merge';
+
+type CardProps = PropsWithChildren<{
+  className?: string;
+}>;
+
+export function Card({ className, children }: CardProps) {
+  return (
+    <article className={twMerge("bg-gray-800 rounded-3xl relative overflow-hidden z-0 after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none p-6", className)}>
+      <div className="absolute inset-0 -z-10 opacity-5" style={{
+        backgroundImage: `url(${grainImage.src})`
+      }} />
+      {children}
+    </article>
+  )
+}
