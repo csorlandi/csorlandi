603f9d6ea2db530ee7c36ec95267b091d260b241
Claudio Orlandi <claudiosorlandi@gmail.com>
2024-12-01 00:56:00 +0100

    feat: finish testimonials section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
+import { TestimonialsSection } from "@/sections/Testimonials";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroSection />
       <ProjectsSection />
       <TapeSection />
+      <TestimonialsSection />
     </main>
   );
 }
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -3,6 +3,10 @@ import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
 import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
 import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
+import { SectionHeader } from "@/components/SectionHeader";
+import Image from "next/image";
+import grainImage from '@/assets/images/grain.jpg';
+import { Card } from "@/components/Card";
 
 const testimonials = [
   {
@@ -38,5 +42,29 @@ const testimonials = [
 ];
 
 export const TestimonialsSection = () => {
-  return <div>Testimonials Section</div>;
+  return (
+    <section className="pb-16 lg:py-24">
+      <div className="container">
+        <SectionHeader title="What Clients Say about Me" eyebrow="Happy Clients" description="Don&apos;t just take my word for it. See what my clients have to say about my work." />
+        <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex gap-8 flex-none">
+            {testimonials.map(testimonial => (
+              <Card key={testimonial.name} className="max-w-xs md:max-w-md md:p-8">
+                <div className="flex gap-4 items-center">
+                  <div className="size-14 flex-shrink-0 bg-gray-700 inline-flex items-center justify-center rounded-full">
+                    <Image src={testimonial.avatar} alt={testimonial.name} className="max-h-full" />
+                  </div>
+                  <div>
+                    <p className="font-semibold">{testimonial.name}</p>
+                    <span className="text-sm text-white/40">{testimonial.position}</span>
+                  </div>
+                </div>
+                <p className="mt-4 md:mt-6 text-sm md:text-base">{testimonial.text}</p>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
