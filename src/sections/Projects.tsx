04b81538c70f49cdb1211a12c3300f1ce00aa613
Claudio Orlandi <claudiosorlandi@gmail.com>
2024-12-01 00:55:37 +0100

    fix: use section header and card component on project section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import CheckCircleIcon from '@/assets/icons/check-circle.svg';
 import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg'
 import grainImage from '@/assets/images/grain.jpg';
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
 
 const portfolioProjects = [
   {
@@ -48,19 +50,10 @@ const portfolioProjects = [
 export const ProjectsSection = () => {
   return <section className="pb-16 lg:py-24">
     <div className="container">
-      <div className="flex justify-center">
-        <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center text-transparent bg-clip-text">
-          Real-world Results
-        </p>
-      </div>
-      <h2 className="font-serif text-3xl text-center mt-6 md:text-5xl">Featured Projects</h2>
-      <p className="text-center text-white/60 mt-4 md:text-lg max-w-md mx-auto lg:text-xl">See how I transformed concepts into engaging digital experiences.</p>
+      <SectionHeader title="Featured Projects" eyebrow="Real-world Results" description="See how I transformed concepts into engaging digital experiences." />
       <div className="flex flex-col mt-10 gap-20 md:mt-20">
         {portfolioProjects.map(project => (
-          <article key={project.title} className="bg-gray-800 rounded-3xl relative overflow-hidden z-0 after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 after:pointer-events-none md:pt-12 md:px-10 lg:pt-16 lg:px-20">
-            <div className="absolute inset-0 -z-10 opacity-5" style={{
-              backgroundImage: `url(${grainImage.src})`
-            }} />
+          <Card key={project.title} className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20">
             <div className="lg:grid lg:grid-cols-2 lg:gap-16">
               <div className="lg:pb-16">
                 <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex font-bold uppercase tracking-widest text-sm gap-2 text-transparent bg-clip-text">
@@ -89,7 +82,7 @@ export const ProjectsSection = () => {
                 <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none" />
               </div>
             </div>
-          </article>
+          </Card>
         ))}
       </div>
     </div>
