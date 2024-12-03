d87da85528b013acbfb269fc604944c02522f735
Claudio Orlandi <claudiosorlandi@gmail.com>
2024-12-03 19:46:00 +0100

    feat: implement call to action section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { AboutSection } from "@/sections/About";
+import { ContactSection } from "@/sections/Contact";
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
@@ -14,6 +15,7 @@ export default function Home() {
       <TapeSection />
       <TestimonialsSection />
       <AboutSection />
+      <ContactSection />
     </main>
   );
 }
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,24 @@
+import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
+import grainImage from '@/assets/images/grain.jpg';
+
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <section className="container py-12 lg:py-20">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
+        <div className="absolute inset-0 opacity-5 -z-10" style={{
+          backgroundImage: `url(${grainImage.src})`
+        }} />
+        <div className="flex flex-col gap-8 items-center md:flex-row">
+          <div>
+            <h2 className="font-serif text-2xl md:text-3xl">Let&apos;s create something amazing together</h2>
+            <p className="text-sm md:text-base mt-2">Ready to bring your next project to life? Let&apos;s connect and discuss how I can help you achieve your goals.</p>
+          </div>
+            <button type="button" className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border md:flex-none border-gray-900">
+              <span className="font-semibold">Contact Me</span>
+              <ArrowUpRightIcon className="size-4" />
+            </button>
+        </div>
+      </div>
+    </section>
+  )
 };
