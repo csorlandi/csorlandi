49c941dc2129d0d8a5ae3923e80dccde915d3710
Claudio Orlandi <claudiosorlandi@gmail.com>
2024-12-05 13:27:47 +0100

    feat: add vercel page speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Calistoga } from 'next/font/google'
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 const calistoga = Calistoga({ subsets: ['latin'], variable: '--font-serif', weight: ['400']})
@@ -22,7 +23,10 @@ export default function RootLayout({
         inter.variable,
         calistoga.variable,
         "bg-gray-900 text-white antialiased text-balance font-sans"
-      )}>{children}</body>
+      )}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
